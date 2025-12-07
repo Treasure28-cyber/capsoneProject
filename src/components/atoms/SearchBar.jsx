b545512d338afcfbc6c2a374310bdf045971c70c
Treasure Ita <treasureita28@gmail.com>
2025-12-08 10:25:25 +1300

    Fix SearchBar.css import case

--- src/components/atoms/SearchBar.jsx
+++ src/components/atoms/SearchBar.jsx
@@ -1,6 +1,6 @@
 // src/components/atoms/SearchBar.jsx
 import Button from "./Button";
-import "./searchBar.css";
+import "./SearchBar.css";
 import searchIcon from "../../assets/search.png";
 
 const SearchBar = () => {
